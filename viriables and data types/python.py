d09f20162949fdd99ee0f60f01b13054c967c12f
thisafrid <afridmd46@gmail.com>
2026-06-29 19:57:08 +0530

    Initial commit

---
+++ viriables and data types/python.py
@@ -0,0 +1,8 @@
+"""num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+avg = (num1 + num2) / 2
+print(avg)"""
+
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+print("true" if  a>=b else "false")
