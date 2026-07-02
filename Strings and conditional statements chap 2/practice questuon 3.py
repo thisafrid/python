f7f80ce2c96dfc609d3a9da9e978c24c999445e4
thisafrid <afridmd46@gmail.com>
2026-07-02 13:02:45 +0530

    update

--- Strings and conditional statements chap 2/practice questuon 3.py
+++ Strings and conditional statements chap 2/practice questuon 3.py
@@ -1,11 +1,14 @@
-#2.Q find the greatest number among four numbers
+#3.Q find the greatest number among four numbers
 
 num1,num2,num3,num4 = int(input("enter the first number: ")), int(input("enter the second number: ")), int(input("enter the third number: ")), int(input("enter the fourth number: "))
-if num1>=num2 and num2>=3 and num3>=num4:
+if num1==num2==num3==num4:
+    print("all numbers are equal")
+elif num1>=num2 and num2>=3 and num3>=num4:
     print("the first number is the greatest")
 elif num2>=num3 and num3>=num4:
     print("the second number is the greatest")
 elif num3>=num4:
     print("the third number is the greatest")
+
 else:
     print("the fourth number is the greatest")
