9dadc33913087167a0a787092b7d7765a65e9dd6
thisafrid <afridmd46@gmail.com>
2026-07-02 18:45:09 +0530

    update

---
+++ Strings and conditional statements chap 2/practice question 4.py
@@ -0,0 +1,7 @@
+#Q4 - to check whether the number is multiplicable of 7 or not
+
+num = int(input("enter the number: "))
+if num % 7 == 0:
+    print("the number is multiplicable of 7")
+else:
+    print("the number is not multiplicable of 7")
