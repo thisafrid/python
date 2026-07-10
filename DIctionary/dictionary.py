69c670b27d18f0585646983448948ca9d6124051
thisafrid <afridmd46@gmail.com>
2026-07-10 18:58:12 +0530

    update

--- DIctionary/dictionary.py
+++ DIctionary/dictionary.py
@@ -1,8 +1,10 @@
 #used to store data values in key
+#dictionary are mutable(change) and dont allow duplicates
 
 login = {
     "name" : "afrid",
     "rollno" : 27,
     "stud" : "btech"
 }
-print(login)
+print(login["name"])
+print(login["rollno"])
---
+++ DIctionary/nesting in dictionary.py
@@ -0,0 +1,12 @@
+#we can use nesting here also
+#example
+
+school = {
+    "name" : "afrid"
+    "subjects" : {
+        "phy" : 90,
+        "maths" : 77,
+    }
+    "rollno" : 24782
+}
+print(school)
