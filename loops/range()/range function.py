ca856c7e9517eea715eb759b88cd8ae797fde033
thisafrid <afridmd46@gmail.com>
2026-07-15 21:18:22 +0530

    ranges

---
+++ loops/range()/range function.py
@@ -0,0 +1,30 @@
+'''Range functions returns a sequence of numbers, starting from 0 by default, 
+and increments by 1 (by default), and stops before a specified number.'''
+#example 1 :
+
+num = range(5)
+for i in num:
+    print(i)#here the range value will not print like 5 will not print and range starts from 0
+'''output:
+0
+1
+2
+3
+4'''
+
+#example 2 :
+
+#we can use range this way and also we can use 
+for i in range(10):
+    print(i) 
+'''output: 
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9'''
